c07275c602b0bbf27ea4257500df21256822c996
prince954 <akprince0268@gmail.com>
2023-04-05 13:05:12 +0600

    Style: Nav Done

--- assects/js/custom.js
+++ assects/js/custom.js
@@ -1,10 +1,17 @@
  var nav_section = document.querySelector(".nav-section");
  var toggle = document.querySelector("#toggle i");
+ var toggle01 = document.querySelector('.responsive_nav');
 
+ toggle01.addEventListener("click",()=>{
+     nav_section.classList.toggle("active");
+ });
 
  toggle.addEventListener("click", () => {
 
      const isopen = nav_section.classList.toggle("active");
 
      toggle.classList = isopen ? 'ph ph-x' : 'ph ph-equals';
+
  });
+
+ 
