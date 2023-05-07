0a421876f9e0fc5fb82e04522f19163115899f33
prince954 <akprince0268@gmail.com>
2023-05-07 14:54:38 +0600

    Style:menu-item Scrollbar Hide

--- assects/js/custom.js
+++ assects/js/custom.js
@@ -1,17 +1,18 @@
- var nav_section = document.querySelector(".nav-section");
- var toggle = document.querySelector("#toggle i");
+var nav_section = document.querySelector(".nav-section");
+ var toggle = document.querySelector("#toggle .i");
  var toggle01 = document.querySelector('.responsive_nav');
 
  toggle01.addEventListener("click",()=>{
      nav_section.classList.toggle("active");
- });
-
- toggle.addEventListener("click", () => {
-
-     const isopen = nav_section.classList.toggle("active");
-
      toggle.classList = isopen ? 'ph ph-x' : 'ph ph-equals';
-
  });
+
+//  toggle.addEventListener("click", () => {
+
+//      const isopen = nav_section.classList.toggle("active");
+
+//      toggle.classList = isopen ? 'ph ph-x' : 'ph ph-equals';
+
+//  });
 
  
